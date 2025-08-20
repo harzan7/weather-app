a0681da1c65904f792094870bb6c796944b294b2
Harza <harzan12345@gmail.com>
2025-08-20 19:07:09 -0400

    Add initial html UI for app, with dummy data (Dallas weather).

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,38 @@ import React, {useState} from 'react';
 import axios from 'axios';
 
 function App() {
-
+    //const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=9add0191e3ef2fa43fd9dec356ed7f36`;
   return (
-    <>
-      <h1>Hello</h1>
-    </>
-  )
+    <div className={'app'}>
+        <div className={'container'}>
+            <div className={'top'}>
+                <div className={'location'}>
+                    <p>Dallas</p>
+                </div>
+                <div className={'temp'}>
+                    <h1>60&deg;F</h1>
+                </div>
+                <div className={'description'}>
+                    <p>Clouds</p>
+                </div>
+            </div>
+            <div className={'bottom'}>
+                <div className={'feels'}>
+                    <p className={'bold'}>65&deg;F</p>
+                    <p>Feels Like</p>
+                </div>
+                <div className={'humidity'}>
+                    <p className={'bold'}>20%</p>
+                    <p>Humidity</p>
+                </div>
+                <div className={'wind'}>
+                    <p className={'bold'}>12 MPH</p>
+                    <p>Wind Speed</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
 }
 
 export default App
