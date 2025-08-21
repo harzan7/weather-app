a5df1fac9f3c2742fccf3cfa1dc812a1cd12650c
Harza <harzan12345@gmail.com>
2025-08-21 19:59:33 -0400

    Make weather data not be visible initially, until the user inputs a location.

--- src/App.jsx
+++ src/App.jsx
@@ -40,20 +40,23 @@ function App() {
                     {data.weather ? <p>{data.weather[0].main}</p> : null}
                 </div>
             </div>
-            <div className={'bottom'}>
-                <div className={'feels'}>
-                    {data.main ? <p className={'bold'}>{data.main.feels_like}&deg;F</p> : null}
-                    <p>Feels Like</p>
+
+            {data.name != undefined &&
+                <div className={'bottom'}>
+                    <div className={'feels'}>
+                        {data.main ? <p className={'bold'}>{data.main.feels_like}&deg;F</p> : null}
+                        <p>Feels Like</p>
+                    </div>
+                    <div className={'humidity'}>
+                        {data.main ? <p className={'bold'}>{data.main.humidity}%</p> : null}
+                        <p>Humidity</p>
+                    </div>
+                    <div className={'wind'}>
+                        {data.wind ? <p className={'bold'}>{data.wind.speed} MPH</p> : null}
+                        <p>Wind Speed</p>
+                    </div>
                 </div>
-                <div className={'humidity'}>
-                    {data.main ? <p className={'bold'}>{data.main.humidity}%</p> : null}
-                    <p>Humidity</p>
-                </div>
-                <div className={'wind'}>
-                    {data.wind ? <p className={'bold'}>{data.wind.speed} MPH</p> : null}
-                    <p>Wind Speed</p>
-                </div>
-            </div>
+            }
         </div>
     </div>
     );
