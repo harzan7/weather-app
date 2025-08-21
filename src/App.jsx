0c6cfb31c5ed800f7b860ba70b8f2bfb72e26719
Harza <harzan12345@gmail.com>
2025-08-21 19:46:45 -0400

    Add search feature.

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,32 @@ import React, {useState} from 'react';
 import axios from 'axios';
 
 function App() {
-    //const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=9add0191e3ef2fa43fd9dec356ed7f36`;
-  return (
+    const [data, setData] = useState({});
+    const [location, setLocation] = useState('');
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=9add0191e3ef2fa43fd9dec356ed7f36`;
+
+    const searchLocation = (event) => {
+        if (event.key === 'Enter') {
+            axios.get(url).then((response) => {
+                setData(response.data);
+                console.log(response.data);
+            })
+            setLocation('')
+        }
+    }
+
+    return (
     <div className={'app'}>
+        <div className={'search'}>
+            <input
+                value={location}
+                onChange={event => setLocation(event.target.value)}
+                onKeyDown={searchLocation}
+                placeholder="Enter location"
+                type={'text'}
+            />
+        </div>
         <div className={'container'}>
             <div className={'top'}>
                 <div className={'location'}>
@@ -33,7 +56,7 @@ function App() {
             </div>
         </div>
     </div>
-  );
+    );
 }
 
 export default App
